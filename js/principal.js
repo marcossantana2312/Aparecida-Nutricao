984968762c7f3d3c7214ebc6cc276f73586e1d4d
unknown <marcos.santana231296@gmail.com>
2019-01-05 21:39:37 -0300

    Adicionado validação de IMC e destaque de erro de peso ou altura

--- js/principal.js
+++ js/principal.js
@@ -2,22 +2,26 @@ var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 console.log(titulo.textContent);
 
-var paciente = document.querySelector("#primeiro-paciente");
-console.log(paciente);
+var pacientes = document.querySelectorAll(".paciente");
+console.log(pacientes);
 
+function validacaoImc(paciente){
+    var altura = paciente.querySelector(".info-altura").textContent;
+    var peso = paciente.querySelector(".info-peso").textContent;
 
-var altura = paciente.querySelector(".info-altura").textContent;
-var peso = paciente.querySelector(".info-peso").textContent;
+    var tdImc = paciente.querySelector(".info-imc");
 
-var tdImc = paciente.querySelector(".info-imc");
-
-if(peso <=0 || peso > 200){
-    console.log("Peso inválido");
-    tdImc.textContent = "Peso inválido";
-}else if(altura <=0 || altura > 2.5){
-    console.log("Altura inválida");
-    tdImc.textContent = "Altura inválida"
-}else{
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
+    if(peso <=0 || peso > 200){
+        console.log("Peso inválido");
+        tdImc.textContent = "Peso inválido";
+        paciente.classList.add("paciente-invalido");
+    }else if(altura <=0 || altura > 2.5){
+        console.log("Altura inválida");
+        tdImc.textContent = "Altura inválida"
+        paciente.classList.add("paciente-invalido");
+    }else{
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
 }
+pacientes.forEach(validacaoImc);
